72570e45209f8cac7f879752e0453d478a903adb
maruthi0209 <sethumaruthi93@gmail.com>
2025-03-11 22:36:18 +0530

    removed extra code

--- cart.js
+++ cart.js
@@ -28,52 +28,6 @@ window.addEventListener("load", () => {
     populateFooterSection();
 });
 
-// function populateHeaderSection() {
-//     let headerSection = document.getElementById("header");
-//     let logoContainer = document.createElement("div");
-//     logoContainer.id = "logoContainer";
-//     logoContainer.innerHTML = "<img src='./MyRecipeFinder.png'>"
-//     logoContainer.addEventListener("click", () => {
-//         window.location.href = "./home.html";
-//     });
-//     let searchbarContainer = document.createElement("div");
-//     searchbarContainer.id = "searchContainer";
-//     let searchBar = document.createElement("input");
-//     searchBar.id = "searchbar";
-//     searchBar.type = "text";
-//     searchBar.placeholder = "Search ingredient or category";
-//     let searchButton = document.createElement("button");
-//     searchButton.id = "searchButton";
-//     searchButton.innerHTML = `<ion-icon name="search-outline"></ion-icon>`;
-//     searchbarContainer.append(searchBar, searchButton);
-//     let navLinksContainer = document.createElement("div");
-//     navLinksContainer.id = "navLinksContainer";
-//     let cartLink = document.createElement("a");
-//     cartLink.href = "./home.html";
-//     cartLink.innerHTML = `<img src="./home.jpg">`; // https://cdn-icons-png.flaticon.com/512/3839/3839530.png
-//     let userIcon = document.createElement("a");
-//     userIcon.href = "./index.html";
-//     userIcon.innerHTML = `<img src="./logout.png">`; // https://img.icons8.com/?size=100&id=JesOX3f2LVdM&format=png&color=000000
-//     navLinksContainer.append(cartLink, userIcon);
-//     let navLinkCheckBox = document.createElement("input");
-//     navLinkCheckBox.type = "checkbox";
-//     navLinkCheckBox.id = "navLinkCheckBox";
-//     navLinkCheckBox.setAttribute("name", "checkbox");
-//     let navLinkCheckBoxLabel = document.createElement("label");
-//     navLinkCheckBoxLabel.id = "navLinkCheckBoxLabel";
-//     navLinkCheckBoxLabel.setAttribute("for", "navLinkCheckBox");
-//     navLinkCheckBoxLabel.innerHTML = `<ion-icon name="menu-outline" class="menu-outline"></ion-icon>`;
-//     let searchCheckBox = document.createElement("input");
-//     searchCheckBox.type = "checkbox";
-//     searchCheckBox.id = "searchCheckBox";
-//     searchCheckBox.setAttribute("name", "searchCheckbox");
-//     let searchCheckBoxLabel = document.createElement("label");
-//     searchCheckBoxLabel.id = "searchCheckBoxLabel";
-//     searchCheckBoxLabel.setAttribute("for", "searchCheckBox");
-//     searchCheckBoxLabel.innerHTML = `<ion-icon name="search-outline" class="search-outline"></ion-icon`;
-//     headerSection.append(logoContainer, navLinkCheckBox, searchCheckBox, searchbarContainer, searchCheckBoxLabel, navLinksContainer, navLinkCheckBoxLabel);
-// }
-
 function populateMainSection() {
     let mainSection = document.getElementById("mainContainer");
     let searchModal = returnModal("searchModal");
@@ -204,33 +158,3 @@ let getItemsTableRows = function (cook, cartTotalAmountColumn, cartTotalQuantity
     itemsTableRow.append(itemsTableBook, itemsTablePrice, itemsTableQuantity, itemsTableIndividualCost);
     return itemsTableRow;    
 }
-
-// function populateFooterSection() {
-//     let footerSection = document.getElementById("footer");
-//     let aboutContainer = document.createElement("div");
-//     aboutContainer.id = "aboutContainer";
-//     let aboutContentHeading = document.createElement("h2");
-//     aboutContentHeading.classList.add("corinthia-bold");
-//     aboutContentHeading.innerText = "About Me";
-//     aboutContent = document.createElement("p");
-//     aboutContent.id = "aboutContent";
-//     aboutContent.classList.add("dancing-script-text");
-//     aboutContent.innerText = "Hi, My name is Sethu Maruthi and I'm the creator of My Recipe Finder. I like cooking various recipes that I come across my travels to various new places. Hope you find a recipe that touches your heart and fills you with joy. " + ``;
-//     aboutContainer.append(aboutContentHeading, aboutContent);
-//     let contactContainer = document.createElement("div");
-//     contactContainer.id = "contactContainer";
-//     let contactContentHeading = document.createElement("h2");
-//     contactContentHeading.classList.add("corinthia-bold");
-//     contactContentHeading.innerText = "Contact Me";
-//     contactContent = document.createElement("p");
-//     contactContent.id = "contactContent";
-//     contactContent.classList.add("dancing-script-text");
-//     contactContent.innerText = "You can reach out to me at sethumaruthi93@gmail.com."
-//     contactContainer.append(contactContentHeading, contactContent);
-//     let copyright = document.createElement("div");
-//     copyright.id = "copyright";
-//     copyright.innerHTML = ` &copy; Copyright 2025. All rights reserved.`;
-
-//     footerSection.append(aboutContainer, contactContainer, copyright);
-// }
-
